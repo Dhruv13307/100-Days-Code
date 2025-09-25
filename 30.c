1adfd673a0d5bdd5a2c21c31411079c31066dd38
Dhruv <dhruvaggarwal1857@gmail.com>
2025-09-26 02:20:10 +0530

    Update 30.c

--- 30.c
+++ 30.c
@@ -1,3 +1,19 @@
+// Q30: Write a program to reverse a given number.
+
+// /*
+// Sample Test Cases:
+// Input 1:
+// 1234
+// Output 1:
+// 4321
+
+// Input 2:
+// 100
+// Output 2:
+// 1
+
+// */
+
 #include <stdio.h>
 int main() {
     int n, rev = 0, digit;
@@ -11,3 +27,4 @@ int main() {
     printf("Reversed number = %d\n", rev);
     return 0;
 }
+
