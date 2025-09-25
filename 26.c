495b33b1317b5f34ebea90ecb89c02b024e35c7f
Dhruv <dhruvaggarwal1857@gmail.com>
2025-09-26 02:04:53 +0530

    Update 26.c

--- 26.c
+++ 26.c
@@ -1,3 +1,19 @@
+// Q26: Write a program to print numbers from 1 to n.
+
+// /*
+// Sample Test Cases:
+// Input 1:
+// 5
+// Output 1:
+// 1 2 3 4 5
+
+// Input 2:
+// 3
+// Output 2:
+// 1 2 3
+
+// */
+
 #include <stdio.h>
 int main() {
     int n;
@@ -7,3 +23,4 @@ int main() {
         printf("%d ", i);
     return 0;
 }
+
