6b43a8b9c525a130a26285402da6a05f98557ac6
Dhruv <dhruvaggarwal1857@gmail.com>
2025-10-13 19:37:04 +0530

    Update 27.c

--- 27.c
+++ 27.c
@@ -19,9 +19,10 @@ int main() {
     int n, sum = 0;
     printf("Enter n: ");
     scanf("%d", &n);
-    for(int i=1; i<n; i+=2,)
-        sum += i;
+    for(int i=1; i<=n; i++)
+        sum = sum+(2*i-1);
     printf("Sum of first %d odd numbers = %d\n", n, sum);
     return 0;
 }
+
 
